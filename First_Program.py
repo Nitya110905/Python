04c5a4b9009413a817af756b0dac473f15a62727
Nitya Shah <ncs110905@gmail.com>
2025-06-28 17:24:39 +0530

    First Python Program

---
+++ First_Program.py
@@ -0,0 +1,7 @@
+# - Python is a high-level,interpretd,multi-purpose and object oriented programming language.
+
+# -Guido Van Rossume founded Python on 20th Feb,1991.
+
+# -Case Sensitive
+
+print("Hello Python")
