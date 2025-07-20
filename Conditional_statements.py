2e8ec0e27115282771afc2fc5379ee65d7cee6e0
Nitya Shah <ncs110905@gmail.com>
2025-07-20 22:35:52 +0530

    Conditional Statements

---
+++ Conditional_statements.py
@@ -0,0 +1,76 @@
+
+# Voting System
+# age = int(input("Enter Your Age:"))
+# if age>100:
+#     print("Not valid")
+# elif age>=18:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+
+# Greatest of two no.
+# num1 = int(input("Enter first no."))
+# num2 = int(input("Enter Second no."))
+
+# if num1>num2:
+#     print("Number:",num1,"is greater than",num2)
+# else:
+#         print("Number:",num2,"is greater than",num1)
+
+
+# Greatest of three no.
+# num1 = int(input("Enter first no."))
+# num2 = int(input("Enter Second no."))
+# num3 = int(input("Enter Third no."))
+
+# if num1>num2:
+#     if num1>num3:
+#         print("Greatest no. is: ",num1)
+
+# elif num2>num3:
+#     print("Greatest no. is",num2)
+
+# else:
+#     print("Greatest no. is",num3)
+
+# n1 = int(input("Enter First No. :"))
+# n2 = int(input("Enter second No. :"))
+# n3 = int(input("Enter third No. :"))
+
+# if n1>n2 and n1>n3:
+#     print(n1,"is greatest")
+# elif n2>n1 and n2>n3:
+#     print(n2,"is greatest")
+# else:
+#     print(n3,"is greatest")
+
+
+# A simple task
+# print("Welcome to Python Dabeli-Vadapav Stall")
+# print("Dabeli (1pc-In Oil)  20/-    Dabeli(1pc-In Butter) 30/-")
+# print("Vadapav (1pc-In Oil)  20/-     Vadapav(1pc-In Butter) 30/-")
+# num = int(input("Press 1 to opt for Dabeli and press 2 for VadaPav"))
+# if num ==1:
+#     a = int(input("Press 3 for Oil and 4 for Butter"))
+#     if a == 3:
+#         b = int(input("Enter the Quantity of Dabeli:"))
+#         print("Your order is ",b," Dabelies")
+#         print("Your Bill Amt. ",b*20," Rupees Only")
+
+#     elif a == 4:
+#         b = int(input("Enter the Quantity:"))
+#         print("Your order is ",b," Butter Dabelies")
+#         print("Your Bill Amt. ",b*30," Rupees Only")
+
+# elif num == 2:
+#     a = int(input("Press 3 for Oil and 4 for Butter"))
+#     if a == 3:
+#         b = int(input("Enter the Quantity:"))
+#         print("Your order is ",b," Vadapavs")
+#         print("Your Bill Amt. ",b*20," Rupees Only")
+
+#     elif a == 4:
+#         b = int(input("Enter the Quantity of Vadapav:"))
+#         print("Your order is ",b," Butter Vadapavs")
+#         print("Your Bill Amt. ",b*30," Rupees Only")
